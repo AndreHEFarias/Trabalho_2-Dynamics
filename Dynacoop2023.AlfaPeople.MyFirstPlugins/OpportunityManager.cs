859e4185398cf7dcd5afc13bed629711b09afea5
Andre <andrefalconblue@gmail.com>
2023-02-23 23:29:32 -0300

    Removendo codigo não utilizado

--- Dynacoop2023.AlfaPeople.MyFirstPlugins/OpportunityManager.cs
+++ Dynacoop2023.AlfaPeople.MyFirstPlugins/OpportunityManager.cs
@@ -1,12 +1,6 @@
 ﻿using Dynacoop2023.AlfaPeople.ConsoleApplication.Controller;
-using Dynacoop2023.AlfaPeople.ConsoleApplication.Model;
 using Microsoft.Xrm.Sdk;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Services.Description;
 
 namespace Dynacoop2023.AlfaPeople.MyFirstPlugins
 {
